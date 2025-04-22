a9787a052f031ab4f80aa27bffbf6c57b9f68e45
Thomascogez <thomas.cogez@gmail.com>
2025-04-22 22:15:04 +0200

    chore: some tweak

--- app/[handle]/page.tsx
+++ app/[handle]/page.tsx
@@ -8,6 +8,8 @@ export default async function HandlePage(props: { params: Promise<{ handle: stri
     const endpoint = new URL(process.env.VERCEL_URL ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}` : "http://localhost:3000");
     endpoint.pathname = `/api/analyze-profile/${handle}`;
 
+    console.log(endpoint);
+
     const response = await fetch(endpoint, {
         method: "POST",
     })
