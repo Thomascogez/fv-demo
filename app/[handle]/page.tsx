15d2565427d9d8549e6b957a6a6c5b5540bc3b33
Thomascogez <thomas.cogez@gmail.com>
2025-04-22 22:28:02 +0200

    chore: display decoded uri component

--- app/[handle]/page.tsx
+++ app/[handle]/page.tsx
@@ -7,8 +7,10 @@ import { type TweetWithAiAnalysis } from "@/lib/ai";
 export default async function HandlePage(props: { params: Promise<{ handle: string }> }) {
     const { handle } = await props.params;
 
+    const decodedHandle = decodeURIComponent(handle);
+
     const endpoint = new URL(process.env.VERCEL_URL ? `https://${process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL}` : "http://localhost:3000");
-    endpoint.pathname = `/api/analyze-profile/${handle}`;
+    endpoint.pathname = `/api/analyze-profile/${decodedHandle}`;
 
     const response = await fetch(endpoint, {
         method: "POST",
@@ -24,10 +26,10 @@ export default async function HandlePage(props: { params: Promise<{ handle: stri
         <main className="flex justify-center items-center p-8">
             <div className="flex flex-col items-center justify-center gap-4 w-full max-w-xl">
                 <h1 className="text-4xl font-bold">
-                    {decodeURIComponent(handle)}
+                    {decodedHandle}
                 </h1>
                 <p className="text-lg">
-                    Showing latest tweets from {handle}
+                    Showing latest tweets from {decodedHandle}
                 </p>
                 <div className="flex flex-col gap-4">
                     {tweetsWithAiAnalysis.map((tweet, index) => <TweetCard key={`${tweet.date}-${index}`} tweet={tweet} />)}
